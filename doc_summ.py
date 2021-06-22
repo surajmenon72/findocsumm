b72dd9234f05213db6b4f55f3e7ebf6f16166013
Suraj Menon <sjmickiemouse@gmail.com>
2021-06-22 14:42:12 -0700

    Works pretty well for headers.

--- doc_summ.py
+++ doc_summ.py
@@ -22,38 +22,43 @@ def find_date(results, result):
 
 	((start_X, start_Y, end_X, end_Y), text) = result
 
-	matches = datefinder.find_dates(text) 
+	matches = datefinder.find_dates(text)
+
 	#assume only one match TODO: make this more general
-	year_found = False
-	dates = []
-	for match in matches:
-		day = match.day
-		month = match.month
-		year = match.year
+	try:
+		year_found = False
+		dates = []
+		for match in matches:
+			day = match.day
+			month = match.month
+			year = match.year
 
-		#ok, we need to find the year, TODO: make this smarter
-		#for now, just see if there is a date below
-		for ((start_Xy, start_Yy, end_Xy, end_Yy), text) in results:
+			#ok, we need to find the year, TODO: make this smarter
+			#for now, just see if there is a date below
+			for ((start_Xy, start_Yy, end_Xy, end_Yy), text) in results:
 
-			start_diff_x = np.abs(start_Xy - start_X)
-			end_diff_x = np.abs(end_Xy - end_X)
-			start_diff_y = start_Yy - start_Y
-			end_diff_y = end_Yy - end_Y
-			total_diff_x = start_diff_x + end_diff_x
-			total_diff_y = start_diff_y + end_diff_y
+				start_diff_x = np.abs(start_Xy - start_X)
+				end_diff_x = np.abs(end_Xy - end_X)
+				start_diff_y = start_Yy - start_Y
+				end_diff_y = end_Yy - end_Y
+				total_diff_x = start_diff_x + end_diff_x
+				total_diff_y = start_diff_y + end_diff_y
 
-			if ((total_diff_y > 0) and (total_diff_y < date_y_threshold) and (total_diff_x < date_x_threshold)):
-				new_text = text.split() 
-				for t in new_text:
-					to = t.translate(str.maketrans('', '', string.punctuation))
-					try:
-						pot_year = int(to)
-						if ((pot_year > year_beg) and (pot_year < year_end)):
-							year = pot_year
-							dates.append((day, month, year))
-							year_found = True
-					except:
-						year = year
+				if ((total_diff_y > 0) and (total_diff_y < date_y_threshold) and (total_diff_x < date_x_threshold)):
+					new_text = text.split() 
+					for t in new_text:
+						#to = t.translate(str.maketrans(' ', ' ', string.punctuation))
+						to = t.translate(str.maketrans({key: " ".format(key) for key in string.punctuation}))
+						try:
+							pot_year = int(to)
+							if ((pot_year > year_beg) and (pot_year < year_end)):
+								year = pot_year
+								dates.append((day, month, year))
+								year_found = True
+						except:
+							year = year
+	except:
+		print ('No Date')
 
 	return year_found, dates
 
@@ -61,9 +66,16 @@ def get_closeness(result, n_result):
 	((start_X, start_Y, end_X, end_Y), text) = result
 	((start_Xn, start_Yn, end_Xn, end_Yn), textn) = n_result
 
-	word_thresh = 25 #TODO: Tune this better, and fix this whole distance system
+	#word_thresh = 5 #TODO: Tune this better, and fix this whole distance system
+	frac_thresh = .3 #3 distance per 10 chars
 
-	dist = Levenshtein.distance(text, textn)
+	len1 = len(text)
+	len2 = len(textn)
+	min_len = np.minimum(len1, len2)
+
+	word_thresh = frac_thresh*min_len
+
+	dist = Levenshtein.distance(text, textn) #TODO: probably is a better way to do this
 	if (dist < word_thresh):	
 		startx_diff = np.abs(start_X - start_Xn)
 		endx_diff = np.abs(end_X - end_Xn)
@@ -98,15 +110,34 @@ def get_similar_items(results, threshold=10):
 
 	return sim_results
 
+def print_similar(results, similar):
+	i = 1
+	for s in similar:
+		print ('List')
+		print (i)
+		for k in range(len(s)):
+			print (results[s[k]])
+
+		i += 1
+
+def print_headers(headers):
+	ordered_headers = sorted(headers, key=lambda tup: tup[0][1])
+
+	print ('Printing Ordered Headers')
+	for oh in ordered_headers:
+		((start_X, start_Y, end_X, end_Y), text) = oh
+		print (text)
+
 #TODO: This func sucks, fix it to make it more efficient
-def delete_similar_items(results, sim_items):
+def delete_similar_headers(results, sim_items):
 	d = enchant.Dict("en_US")
 	results_new = []
+	results_final = []
 	real_thresh = .75 #TODO: Tune this
 	for l in sim_items:
 		#keep the item that has the most real words
 		most_real_words = 0
-		best_i = 1e6
+		best_i = 0
 		for i in range(len(l)):
 			index = l[i]
 			result = results[index]
@@ -136,20 +167,39 @@ def delete_similar_items(results, sim_items):
 		if (found == False):
 			result_to_copy = results[i]
 			((start_X, start_Y, end_X, end_Y), text) = result_to_copy
+			results_new.append(result_to_copy)
 
-			#check percentage of real words
-			words = text.split()
-			num_words = len(words)
-			num_real_words = 0
-			for word in words:
-				if (d.check(str(word))):
-					num_real_words += 1
 
+	#now cleanup, remove punctuation, and remove those with too few real words
+	for i in range(len(results_new)):
+		result_to_process = results_new[i]
+		((start_X, start_Y, end_X, end_Y), text) = result_to_process
+
+		#removes punctuation
+		#clean_text = text.translate(str.maketrans(' ', ' ', string.punctuation))
+		clean_text = text.translate(str.maketrans({key: " ".format(key) for key in string.punctuation}))
+
+		#check percentage of real words
+		words = clean_text.split()
+		num_words = len(words)
+		num_real_words = 0
+		for word in words:
+			if (d.check(str(word))):
+				num_real_words += 1
+
+		if (num_words > 0):
 			real_perc = (num_real_words/num_words)
-			if (real_perc >= real_thresh):
-				results_new.append(result_to_copy)
+		else:
+			real_perc = 0
 
-	return results_new
+		if (real_perc >= real_thresh):
+			result_to_append = ((start_X, start_Y, end_X, end_Y), clean_text)
+			results_final.append(result_to_append)
+
+	return results_final
+
+def delete_similar_dates(results, dates, sim_items):
+	print ('Deleting Similar Dates')
 
 def get_crosshair_distance(header, date, count):
 	((start_X, start_Y, end_X, end_Y), text) = header
@@ -225,7 +275,9 @@ def print_results(headers, dates, dates_full, results):
 #Creating argument dictionary for the default arguments needed in the code. 
 args = {"full_image":"/Users/surajmenon/Desktop/findocDocs/apple_tc_full1.png","east":"/Users/surajmenon/Desktop/findocDocs/frozen_east_text_detection.pb", "min_confidence":0.5, "width":320, "height":320}
 
-args['full_image']="/Users/surajmenon/Desktop/findocDocs/apple_tc_full1.png"
+#args['full_image']="/Users/surajmenon/Desktop/findocDocs/apple_tc_full1.png" #apple
+#args['full_image']="/Users/surajmenon/Desktop/findocDocs/cat_tc_full1.png" #cat
+args['full_image']="/Users/surajmenon/Desktop/findocDocs/mcds_tc_full1.png" #mcds
 args['east']="/Users/surajmenon/Desktop/findocDocs/frozen_east_text_detection.pb"
 args['min_confidence'] = 1e-3 #TODO: tune this
 args['width'] = 320 #TODO: verify these
@@ -274,7 +326,7 @@ for i in range(num_vert_slices):
 		image_to_process = split_images[index]
 
 		#calculate offset, TODO: Verify this works even though we have the buffer
-		X_offset = j*sub_image_width
+		X_offset = j*sub_image_width 
 		Y_offset = i*sub_image_height
 
 		if (j < process_short_threshold):
@@ -303,18 +355,33 @@ for i in range(num_vert_slices):
 		full_results += results
 
 #Now clean up results, remove excess headers and dates, add spellcheck
-header_threshold = 15
-date_threshold = 15
-count_threshold = 15
+#TODO: Tune these values
+header_threshold = 300
+date_threshold = 150
+count_threshold = 50
 
 sim_headers = get_similar_items(header_results, threshold=header_threshold)
 sim_dates = get_similar_items(date_results, threshold=date_threshold)
 sim_counts = get_similar_items(count_results, threshold=count_threshold)
 
-#now delete items based on some metric, for now just get rid of headers
-trim_headers = delete_similar_items(header_results, sim_headers)
+print ('Similar Pieces')
+print (sim_headers)
+print (sim_dates)
+print (sim_counts)
 
-#add something here to remove excess dates, random crosshair check
+#print (header_results)
+#print_similar(header_results, sim_headers)
+
+#now delete items based on some metric, for now just get rid of headers
+trim_headers = delete_similar_headers(header_results, sim_headers)
+
+#add something here to remove excess dates
+trim_dates = delete_similar_dates(date_results, dates_parsed, sim_dates)
+
+#delete headers and dates that don't have crosshairs
+
+#print headers in order
+print_headers(trim_headers)
 
 print ('Current Output')
 print (trim_headers)

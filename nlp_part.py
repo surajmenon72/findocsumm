d2e57076a4c3b2e4a7b7e46c93c0b79e06756ce0
Suraj Menon <sjmickiemouse@gmail.com>
2021-07-05 16:04:43 -0700

    Adding base file for Sentence Embeddings.

---
+++ nlp_part.py
@@ -0,0 +1,7 @@
+import sister
+
+buckets_headers = []
+buckets_counts = ['Total Net Sales', 'Elegy of Emptiness']
+
+def bucket_results(headers, dates, dates_full, counts, date_contexts, count_contexts, clean_results):
+

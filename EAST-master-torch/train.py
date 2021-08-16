8c6868e7c4b117b5e156ce7f6f039e1ca9780941
Suraj Menon <sjmickiemouse@gmail.com>
2021-08-16 12:25:03 -0700

    Training.

--- EAST-master-torch/train.py
+++ EAST-master-torch/train.py
@@ -66,10 +66,10 @@ def train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers,
 
 
 if __name__ == '__main__':
-	#train_img_path = os.path.abspath('/home/surajm72/data/ICDAR_2015/train_img')
-	#train_gt_path  = os.path.abspath('/home/surajm72/data/ICDAR_2015/train_gt')
-	train_img_path = os.path.abspath('/Users/surajmenon/Desktop/findocsumm/data/ICDAR_2015/train_img')
-	train_gt_path  = os.path.abspath('/Users/surajmenon/Desktop/findocsumm/data/ICDAR_2015/train_gt')
+	train_img_path = os.path.abspath('/home/surajm72/data/ICDAR_2015/train_img')
+	train_gt_path  = os.path.abspath('/home/surajm72/data/ICDAR_2015/train_gt')
+	#train_img_path = os.path.abspath('/Users/surajmenon/Desktop/findocsumm/data/ICDAR_2015/train_img')
+	#train_gt_path  = os.path.abspath('/Users/surajmenon/Desktop/findocsumm/data/ICDAR_2015/train_gt')
 	pths_path      = './pths'
 	#batch_size     = 24
 	batch_size 	   = 16

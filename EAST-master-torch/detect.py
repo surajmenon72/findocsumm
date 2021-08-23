1a09f38c98962d54b784e19018789486b159500f
Suraj Menon <sjmickiemouse@gmail.com>
2021-08-22 23:01:49 -0700

    testing Adobe.

--- EAST-master-torch/detect.py
+++ EAST-master-torch/detect.py
@@ -182,12 +182,12 @@ def detect_dataset(model, device, test_img_path, submit_path):
 
 
 if __name__ == '__main__':
-	img_path 	= 'test_img/apple_tc_full1.jpg'
-	#img_path 	= 'test_img/adobe_tc_full2.jpg'
+	#img_path 	= 'test_img/apple_tc_full1.jpg'
+	img_path 	= 'test_img/adobe_tc_full2.jpg'
 	#img_path    = '../data/ICDAR_2015/test_img/img_2.jpg'
 	#model_path  = './pths/east_vgg16.pth'
 	model_path  = './pths/sm4-165.pth'
-	res_img     = './apple.bmp'
+	res_img     = './adobe.bmp'
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 	model = EASTER(False).to(device)
 	model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))

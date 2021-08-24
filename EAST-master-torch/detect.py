29a46d5838ba09f49d07bda59bfa54e0aa5403dc
Suraj Menon <sjmickiemouse@gmail.com>
2021-08-24 13:07:19 -0700

    ..

--- EAST-master-torch/detect.py
+++ EAST-master-torch/detect.py
@@ -204,7 +204,7 @@ test_images = ['test_img2', 'apple_tc_full1', 'adobe_tc_full2']
 
 if __name__ == '__main__':
 	#model_path = './pths/east_vgg16.pth'
-	model_path  = './pths/sm2-300.pth'
+	model_path  = './pths/sm5-50.pth'
 	for t in test_images:
 		img_path = 'test_img/' + t + '.jpg'
 		segs = t.split('_')

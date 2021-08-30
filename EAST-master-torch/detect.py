10d6b5613c6385882960cb01e522cd5e6c3f9843
Suraj Menon <sjmickiemouse@gmail.com>
2021-08-30 16:54:22 -0700

    ..

--- EAST-master-torch/detect.py
+++ EAST-master-torch/detect.py
@@ -217,8 +217,8 @@ def do_detection(img_path, model_path, res_img, scale=4):
 test_images = ['test_img2', 'apple_tc_full1', 'adobe_tc_full2']
 
 if __name__ == '__main__':
-	#model_path = './pths/east_vgg16.pth'
-	model_path  = './pths/EASTER-sm2-460.pth'
+	model_path = './pths/east_vgg16.pth'
+	#model_path  = './pths/EASTER-sm2-460.pth'
 	scale = 2
 	for t in test_images:
 		img_path = 'test_img/' + t + '.jpg'

43ca0ba12dc60d05fdbe7ab9d74b1283dea48000
Suraj Menon <sjmickiemouse@gmail.com>
2021-09-02 18:49:04 -0700

    Try one more finetuning of the pretrained model.

--- EAST-master-torch/detect.py
+++ EAST-master-torch/detect.py
@@ -227,7 +227,7 @@ test_images = ['test_img2', 'apple_tc_full1', 'adobe_tc_full2', 'mcds_tc_full1',
 
 if __name__ == '__main__':
 	#model_path = './pths/east_vgg16.pth'
-	model_path  = './pths/EAST-sm3-ft-50.pth'
+	model_path  = './pths/EAST-sm3-ft-25.pth'
 	scale = 4
 	for t in test_images:
 		img_path = 'test_img/' + t + '.jpg'

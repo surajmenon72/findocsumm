cd49b64bf1462d37843975c349af62594795d42f
Suraj Menon <sjmickiemouse@gmail.com>
2021-09-09 13:41:36 -0700

    ..

--- EAST-master-torch/train.py
+++ EAST-master-torch/train.py
@@ -42,7 +42,7 @@ def train(train_img_path, train_gt_path, test_img_path, test_gt_path, pths_path,
 	model = EASTER(True, True)
 	#model = EAST_STRETCH()
 	#model_name = './pths/east_vgg16.pth'
-	model_name = './pths/EASTER-sm2-420.pth'
+	model_name = './pths/EASTER-sm2-500.pth'
 	model.load_state_dict(torch.load(model_name))
 	epoch_start = 0
 	data_parallel = False

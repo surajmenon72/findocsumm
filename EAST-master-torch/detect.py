3b7c47be4445fb60e9b8b2f47533af7d17c52b4f
Suraj Menon <sjmickiemouse@gmail.com>
2021-09-10 15:00:41 -0700

    ..

--- EAST-master-torch/detect.py
+++ EAST-master-torch/detect.py
@@ -17,8 +17,8 @@ def resize_img(img):
 	w, h = img.size
 
 	#rescale factor
-	#w_scale = 2.75
-	#h_scale = 2.75
+	w_scale = 2.75
+	h_scale = 2.75
 
 	#w_scale = 2.5
 	#h_scale = 2.5
@@ -26,8 +26,8 @@ def resize_img(img):
 	#w_scale = 1.875
 	#h_scale = 1.875
 
-	w_scale = 1
-	h_scale = 1
+	#w_scale = 1
+	#h_scale = 1
 
 	resize_w = int(w * w_scale)
 	resize_h = int(h * h_scale)

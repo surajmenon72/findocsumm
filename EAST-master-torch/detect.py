68683d6ac323a51c296c3e1010781573c7450fd4
Suraj Menon <sjmickiemouse@gmail.com>
2021-09-13 12:16:32 -0700

    ..

--- EAST-master-torch/detect.py
+++ EAST-master-torch/detect.py
@@ -103,7 +103,7 @@ def restore_polys(valid_pos, valid_geo, score_shape, scale=4):
 	return np.array(polys), index
 
 
-def get_boxes(score, geo, score_thresh=0.89, nms_thresh=0.2, scale=4):
+def get_boxes(score, geo, score_thresh=0.895, nms_thresh=0.2, scale=4):
 	'''get boxes from feature map
 	Input:
 		score       : score map from model <numpy.ndarray, (1,row,col)>

76284f19d3c94f8e673b6fd3ea69d36ae0086f02
Suraj Menon <sjmickiemouse@gmail.com>
2021-09-14 15:16:17 -0700

    ..

--- EAST-master-torch/dataset.py
+++ EAST-master-torch/dataset.py
@@ -511,6 +511,9 @@ class custom_dataset(data.Dataset):
 		
 		score_map, geo_map, ignored_map = get_score_geo(img, vertices, labels, self.scale, self.length)
 
+		print (score_map.shape)
+		print (geo_map.shape)
+		exit()
 		boxes = get_boxes(score_map.squeeze(0).cpu().numpy(), geo_map.squeeze(0).cpu().numpy(), scale=self.scale)
 		res_img = plot_boxes(img, boxes)
 		res_img.save('./scale_test.bmp')

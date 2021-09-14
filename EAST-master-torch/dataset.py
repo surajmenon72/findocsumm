e189ea55e7c3bc6ea85ce9dfd5a2d61b8d21b998
Suraj Menon <sjmickiemouse@gmail.com>
2021-09-14 13:54:13 -0700

    ..

--- EAST-master-torch/dataset.py
+++ EAST-master-torch/dataset.py
@@ -461,14 +461,15 @@ class custom_dataset(data.Dataset):
 		img, vertices = rotate_img(img, vertices)
 		img, vertices = crop_img(img, vertices, labels, self.length)
 
-		print (img.shape)
-		print (vertices.shape)
-		exit()
 		transform = transforms.Compose([transforms.ColorJitter(0.5, 0.5, 0.5, 0.25), \
                                         transforms.ToTensor(), \
                                         transforms.Normalize(mean=(0.5,0.5,0.5),std=(0.5,0.5,0.5))])
 		
 		score_map, geo_map, ignored_map = get_score_geo(img, vertices, labels, self.scale, self.length)
+
+		print (transform(img).shape)
+		print (vertices.shape)
+		exit()
 
 		return transform(img), score_map, geo_map, ignored_map
 

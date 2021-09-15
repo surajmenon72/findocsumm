453231c6be4d5699a064cf683bbf3558ee8c8f40
Suraj Menon <sjmickiemouse@gmail.com>
2021-09-15 16:19:42 -0700

    ..

--- EAST-master-torch/train.py
+++ EAST-master-torch/train.py
@@ -44,7 +44,7 @@ def train(train_img_path, train_gt_path, test_img_path, test_gt_path, pths_path,
 	model_name = './pths/east_vgg16.pth'
 	#model_name = './pths/EASTER-sm3-aug3-410.pth'
 	#model_name = './pths/test2/model_epoch_120.pth'
-	#model.load_state_dict(torch.load(model_name))
+	model.load_state_dict(torch.load(model_name))
 	epoch_start = 0
 	data_parallel = False
 	if torch.cuda.device_count() > 1:

a85866e2c72d0492e10813e251772d60e9a3c229
Suraj Menon <sjmickiemouse@gmail.com>
2021-09-23 10:25:23 -0700

    Adding final files to integrate CV.

---
+++ model2_prod.py
@@ -0,0 +1,226 @@
+import torch
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import torch.nn.functional as F
+import math
+
+
+cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+
+
+def make_layers(cfg, batch_norm=False):
+	layers = []
+	in_channels = 3
+	for v in cfg:
+		if v == 'M':
+			layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+		else:
+			conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+			if batch_norm:
+				layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+			else:
+				layers += [conv2d, nn.ReLU(inplace=True)]
+			in_channels = v
+	return nn.Sequential(*layers)
+
+
+class VGG(nn.Module):
+	def __init__(self, features):
+		super(VGG, self).__init__()
+		self.features = features
+		self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+		self.classifier = nn.Sequential(
+			nn.Linear(512 * 7 * 7, 4096),
+			nn.ReLU(True),
+			nn.Dropout(),
+			nn.Linear(4096, 4096),
+			nn.ReLU(True),
+			nn.Dropout(),
+			nn.Linear(4096, 1000),
+		)
+
+		for m in self.modules():
+			if isinstance(m, nn.Conv2d):
+				nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+				if m.bias is not None:
+					nn.init.constant_(m.bias, 0)
+			elif isinstance(m, nn.BatchNorm2d):
+				nn.init.constant_(m.weight, 1)
+				nn.init.constant_(m.bias, 0)
+			elif isinstance(m, nn.Linear):
+				nn.init.normal_(m.weight, 0, 0.01)
+				nn.init.constant_(m.bias, 0)
+
+	def forward(self, x):
+		x = self.features(x)
+		x = self.avgpool(x)
+		x = x.view(x.size(0), -1)
+		x = self.classifier(x)
+		return x
+
+
+class extractor(nn.Module):
+	def __init__(self, pretrained):
+		super(extractor, self).__init__()
+		vgg16_bn = VGG(make_layers(cfg, batch_norm=True))
+		if pretrained:
+			vgg16_bn.load_state_dict(torch.load('./pths/vgg16_bn-6c64b313.pth'))
+		self.features = vgg16_bn.features
+	
+	def forward(self, x):
+		out = []
+		for m in self.features:
+			x = m(x)
+			if isinstance(m, nn.MaxPool2d):
+				out.append(x)
+		#return out[1:]
+		return out[0:]
+
+
+class merge(nn.Module):
+	def __init__(self):
+		super(merge, self).__init__()
+
+		self.conv1 = nn.Conv2d(1024, 128, 1)
+		self.bn1 = nn.BatchNorm2d(128)
+		self.relu1 = nn.ReLU()
+		self.conv2 = nn.Conv2d(128, 128, 3, padding=1)
+		self.bn2 = nn.BatchNorm2d(128)
+		self.relu2 = nn.ReLU()
+
+		self.conv3 = nn.Conv2d(384, 64, 1)
+		self.bn3 = nn.BatchNorm2d(64)
+		self.relu3 = nn.ReLU()
+		self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
+		self.bn4 = nn.BatchNorm2d(64)
+		self.relu4 = nn.ReLU()
+
+		self.conv5 = nn.Conv2d(192, 32, 1)
+		self.bn5 = nn.BatchNorm2d(32)
+		self.relu5 = nn.ReLU()
+		self.conv6 = nn.Conv2d(32, 32, 3, padding=1)
+		self.bn6 = nn.BatchNorm2d(32)
+		self.relu6 = nn.ReLU()
+
+		self.conv7 = nn.Conv2d(96, 32, 1)
+		self.bn7 = nn.BatchNorm2d(32)
+		self.relu7 = nn.ReLU()
+		self.conv8 = nn.Conv2d(32, 32, 3, padding=1)
+		self.bn8 = nn.BatchNorm2d(32)
+		self.relu8 = nn.ReLU()
+
+		self.conv9 = nn.Conv2d(32, 32, 3, padding=1)
+		self.bn9 = nn.BatchNorm2d(32)
+		self.relu9 = nn.ReLU()
+		
+		for m in self.modules():
+			if isinstance(m, nn.Conv2d):
+				nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+				if m.bias is not None:
+					nn.init.constant_(m.bias, 0)
+			elif isinstance(m, nn.BatchNorm2d):
+				nn.init.constant_(m.weight, 1)
+				nn.init.constant_(m.bias, 0)
+
+	def forward(self, x):
+		y = F.interpolate(x[4], scale_factor=2, mode='bilinear', align_corners=True)
+		y = torch.cat((y, x[3]), 1)
+		y = self.relu1(self.bn1(self.conv1(y)))		
+		y = self.relu2(self.bn2(self.conv2(y)))
+		
+		y = F.interpolate(y, scale_factor=2, mode='bilinear', align_corners=True)
+		y = torch.cat((y, x[2]), 1)
+		y = self.relu3(self.bn3(self.conv3(y)))		
+		y = self.relu4(self.bn4(self.conv4(y)))
+		
+		y = F.interpolate(y, scale_factor=2, mode='bilinear', align_corners=True)
+		y = torch.cat((y, x[1]), 1)
+		y = self.relu5(self.bn5(self.conv5(y)))		
+		y = self.relu6(self.bn6(self.conv6(y)))
+
+		y = F.interpolate(y, scale_factor=2, mode='bilinear', align_corners=True)
+		y = torch.cat((y, x[0]), 1)
+		y = self.relu7(self.bn7(self.conv7(y)))		
+		y = self.relu8(self.bn8(self.conv8(y)))
+		
+		y = self.relu9(self.bn9(self.conv9(y)))
+		return y
+
+class output(nn.Module):
+	def __init__(self, scope=512):
+		super(output, self).__init__()
+		self.conv1 = nn.Conv2d(32, 1, 1)
+		self.sigmoid1 = nn.Sigmoid()
+		self.conv2 = nn.Conv2d(32, 4, 1)
+		self.sigmoid2 = nn.Sigmoid()
+		self.conv3 = nn.Conv2d(32, 1, 1)
+		self.sigmoid3 = nn.Sigmoid()
+		self.scope = 512
+		for m in self.modules():
+			if isinstance(m, nn.Conv2d):
+				nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+				if m.bias is not None:
+					nn.init.constant_(m.bias, 0)
+
+	def forward(self, x):
+		score = self.sigmoid1(self.conv1(x))
+		loc   = self.sigmoid2(self.conv2(x)) * self.scope
+		angle = (self.sigmoid3(self.conv3(x)) - 0.5) * math.pi
+		geo   = torch.cat((loc, angle), 1) 
+		return score, geo
+		
+	
+class EASTER(nn.Module):
+	def __init__(self, pretrained=True, retVar=False):
+		super(EASTER, self).__init__()
+		self.extractor = extractor(pretrained)
+		self.merge     = merge()
+		self.output    = output()
+		self.retVar = retVar
+	
+	def forward(self, x, calcVar=False):
+		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+		merge_output = self.merge(self.extractor(x))
+		score, geo = self.output(merge_output)
+
+		if (self.retVar):
+			if (calcVar == True):
+				smooshed_output = torch.reshape(merge_output, (16, 32, 65536))
+				# num_dim = smooshed_output.shape[1]
+				# total_diff = torch.zeros((16, 32, 65536))
+
+				# #Calculating some sort of avg spread
+				# for i in range(num_dim):
+				# 	val = smooshed_output[:, i, :]
+				# 	new_diff = torch.zeros((16, 65536)).to(device)
+				# 	for j in range(num_dim):
+				# 		new_val = smooshed_output[:, j, :]
+				# 		diff = val - new_val
+				# 		diff = torch.square(diff)
+				# 		new_diff += diff
+
+				# 	total_diff[:, i, :] = new_diff
+
+				# diff_mean = torch.mean(total_diff, axis=1)
+				# diff_mean_pix = torch.mean(diff_mean, axis=1)
+				# diff_mean_batch = torch.mean(diff_mean_pix, axis=0)
+
+				smooshed_var = torch.var(smooshed_output, axis=1, unbiased=True)
+				var_full = torch.mean(smooshed_var, dim=1)
+				var_avg = torch.mean(var_full, axis=0)
+
+				return score, geo, var_avg
+				#return score, geo, diff_mean_batch
+			else:
+				var_avg = 0
+				return score, geo, var_avg
+		else:
+			return score, geo
+		
+
+if __name__ == '__main__':
+	m = EASTER()
+	x = torch.randn(1, 3, 256, 256)
+	score, geo = m(x)
+	print(score.shape)
+	print(geo.shape)

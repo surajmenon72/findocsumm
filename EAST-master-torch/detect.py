3ceb3a9911ef31e70e963ff313d10801e7beeb40
Suraj Menon <sjmickiemouse@gmail.com>
2021-10-27 19:07:56 -0700

    ..

--- EAST-master-torch/detect.py
+++ EAST-master-torch/detect.py
@@ -236,7 +236,7 @@ test_images = ['test_img2', 'apple_tc_full1', 'adobe_tc_full2', 'mcds_tc_full1',
 if __name__ == '__main__':
 	#model_path = './pths/east_vgg16.pth'
 	#model_path = './pths/EAST-aug3-100.pth'
-	model_path  = './pths2/EAST-llr-fs-0.5-490.pth '
+	model_path  = './pths2/EAST-llr-fs-0.5-490.pth'
 	model = 'EAST'
 	scale = 4
 	for t in test_images:
